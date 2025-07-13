a73878ffff8cbe3e934d135973f1b0b72940ca8b
Nhan Nguyen <npbaonhan18803@gmail.com>
2025-07-13 16:40:33 +0700

    updated

--- src/test/java/henrynguyen/tests/ErrorValidation.java
+++ src/test/java/henrynguyen/tests/ErrorValidation.java
@@ -11,6 +11,6 @@ public class ErrorValidation extends BaseTest {
     @Test(groups = {"ErrorHandling"}, retryAnalyzer = Retry.class)
     public void errorValidation(){
         landingPage.loginApplication("henry111@gmail.com", "Henry180803");
-        Assert.assertEquals("Incorrect email password.", landingPage.getErrorMessage());
+        Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
     }
 }
